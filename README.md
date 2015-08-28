3b7460e9705aed2326ee109935abcba3c685f1d4
gzwangxuanshu <gzwangxuanshu@corp.netease.com>
2015-08-28 10:35:09 +0800

    first version.
    to do:
    1. delay to search. (not to search immediately when text changed, to prevent of to many search when typing)
    2. number search?

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SWPinYinSearcher
 根据全拼或简拼，搜索汉字，支持多音字。包含汉字转拼音。
+生成用于过滤 Array的Predicate。
+
+用法举例：
+NSPredicate *filter = [SWPinYinSearcher predicateWithEvaluateStringKeyPath:@"name" searchString:@"lgd"];
+NSArray *result = [self.tableDataArray filteredArrayUsingPredicate:filter];
+
+KeyPath：用于过滤的String在Array的Object中的KeyPath。
+searchString：可以是全拼，简拼和汉字，默认忽略所有输入字符中的空白。
+
+例2：
+NSPredicate *filter = [SWPinYinSearcher predicateWithEvaluateStringKeyPath:@"name" searchString:@"lgd" searchOption:SWPinyinSearchOptionsQuanPin | SWPinyinSearchOptionsJianPin | SWPinyinSearchOptionsIgnoreWhiteSpaces];
+
+只搜索拼音，并且忽略输入字符中的空白。
+
---
+++ SWPinYinSearcher/SWPinYinSearcher.h
@@ -0,0 +1,52 @@
+//
+//  SWPinYinSearcher.h
+//  test--tableSearchInContact
+//
+//  Created by SWxs on 8/26/15.
+//  Copyright (c) 2015 SWxs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_OPTIONS(NSUInteger, SWPinyinSearchOptions) {
+    SWPinyinSearchOptionsHanZi              = 1 << 0,       // 汉字匹配
+    SWPinyinSearchOptionsQuanPin            = 1 << 1,       // 全拼匹配
+    SWPinyinSearchOptionsJianPin            = 1 << 2,       // 拼音首字母缩写（简拼）匹配
+    SWPinyinSearchOptionsIgnoreWhiteSpaces  = 1 << 3        // 忽略所有search string中的空白字符
+};
+
+@interface SWPinYinSearcher : NSObject
+
+/**
+ *  匹配汉字/全拼/简拼。如果想只匹配某一个，参见带searchOption的方法
+ *
+ *  @param keyPath       匹配String（如通讯录人名等）在array每个元素中的keyPath。如果array直接就是NSString，请传入nil。
+ *  @param searchString  用户输入的，用于搜索的String。
+ *
+ *  @return              生成Predicate，用于过滤列表。
+ *
+ *  @code
+ *  // code example
+ *  NSPredicate *filter = [SWPinYinSearcher predicateWithEvaluateStringKeyPath:@"name" searchString:@"lgd"];
+ *  NSArray result = [self.tableData filteredArrayUsingPredicate:filter];
+ *  @endcode
+ */
++ (NSPredicate *)predicateWithEvaluateStringKeyPath:(NSString *)keyPath searchString:(NSString *)searchString;
+
+/**
+ *  参见不带searchOption的方法。
+ *  @param option   可以指定匹配 汉字/全拼/拼音缩写 中的某一个或几个。
+ *
+ *  @return              生成Predicate，用于过滤列表。
+ *
+ *  @code
+ *  // code example
+ *  // 只搜缩全拼和拼音缩写：
+ *  NSPredicate *filter = [SWPinYinSearcher predicateWithEvaluateStringKeyPath:@"name" searchString:@"lgd"
+ *  searchOption:SWPinyinSearchOptionsQuanPin | SWPinyinSearchOptionsJianPin];
+ *  NSArray result = [self.tableData filteredArrayUsingPredicate:filter];
+ *  @endcode
+ */
++ (NSPredicate *)predicateWithEvaluateStringKeyPath:(NSString *)keyPath searchString:(NSString *)searchString searchOption:(SWPinyinSearchOptions)option;
+
+@end
